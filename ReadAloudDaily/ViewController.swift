d34fb991700a1f070c5063721d2efced5ad0748a
89Explorer <leral3@naver.com>
2025-03-17 17:16:50 +0900

    Initial Commit

---
+++ ReadAloudDaily/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadAloudDaily
+//
+//  Created by 권정근 on 3/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
